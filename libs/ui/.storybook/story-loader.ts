d9b666eb452861453bb69094d9d1e620066f5db7
Michael Casas <info@nox-technologies.com>
2023-06-14 01:45:03 -0500

    feat: cross platform skia working

--- libs/ui/.storybook/story-loader.ts
+++ libs/ui/.storybook/story-loader.ts
@@ -2,6 +2,7 @@
 // DO NOT EDIT.
 export function loadStories() {
   return [
+    require("libs/ui/src/animated/animated.stories.tsx"),
     require("libs/ui/src/canvas/canvas.stories.tsx"),
     require("libs/ui/src/colors/colors.stories.tsx"),
     require("libs/ui/src/typeography/typeography.stories.tsx"),
